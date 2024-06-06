855c29706ef205b942e278d2c374b6fa0116119a
ulrichgero <49178952+ulrichgero@users.noreply.github.com>
2024-06-06 18:41:22 +0100

    Update triggers-events.sql

--- triggers-events.sql
+++ triggers-events.sql
@@ -1,2 +1,19 @@
 SELECT *
 FROM employees_salary;
+
+SELECT *
+FROM employees_demogra;
+-- triggers and events
+
+DELIMITER $$
+CREATE TRIGGER employee_insert -- trigger name
+	AFTER INSERT ON employees_salary -- table name
+    FOR EACH ROW
+BEGIN
+	INSERT INTO employees_demogra (employee_id, first_name, last_name)
+    VALUE (New.employee_id, New.first_name, New.last_name);
+END $$
+DELIMITER ;
+
+INSERT INTO employees_salary (employee_id, first_name, last_name, salary, role)
+VALUE(21, 'Amel', 'Gero', 75000, 'CEO'); 
